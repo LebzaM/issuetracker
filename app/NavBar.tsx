d954b09620ba22662e49985294b9cd4595f8793c
369webforall <369webforall@gmail.com>
2023-10-25 15:17:51 +0545

    Style navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,18 +1,37 @@
+'use client';
 import React from 'react';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { AiFillBug } from 'react-icons/ai';
+import classnames from 'classnames';
 
 const NavBar = () => {
+  const pathname = usePathname();
+
+  const links = [
+    { id: 1, href: '/', label: 'Dashboard' },
+    { id: 2, href: '/issues', label: 'Issues' },
+  ];
   return (
     <nav className="flex space-x-6 px-5 border-b h-14 mb-5 items-center">
-      <Link href="/">Logo</Link>
+      <Link href="/">
+        <AiFillBug className="text-2xl" />
+      </Link>
 
       <ul className="flex space-x-6">
-        <li>
-          <Link href="/">Dashboard</Link>
-        </li>
-        <li>
-          <Link href="/issues">Issues</Link>
-        </li>
+        {links.map((link) => (
+          <Link
+            href={link.href}
+            key={link.id}
+            className={classnames({
+              'text-zinc-900': link.href === pathname,
+              'text-zinc-500': link.href !== pathname,
+              'hover:text-zinc-800 transition-colors': true,
+            })}
+          >
+            {link.label}
+          </Link>
+        ))}
       </ul>
     </nav>
   );
